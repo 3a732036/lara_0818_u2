a4c517b544a9c73f93ad15e4290e426a62be90a2
Jun-You Ye <3a732036@gm.student.ncut.edu.tw>
2020-10-28 20:47:51 +0800

    練習 1-4~1-5 在 resources/views底下新增 home資料夾、將 resources/views/home.html 重新命名為 index.php並移至 home資料夾內

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// 練習 1-2 設定從 /routes/web.php回傳 home.html
+// 練習 1-2 設定從 /routes/web.php回傳 index.php
 Route::get('home', function (){
     return view('home');
 });
